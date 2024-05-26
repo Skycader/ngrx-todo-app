86fe3c233c2fe164fa7ca8df9d4ab44b4716fbb7
skycader <skycader@yandex.ru>
2024-05-26 22:59:00 +0300

    main: cypress engaged

--- cypress/e2e/todo.spec.cy.ts
+++ cypress/e2e/todo.spec.cy.ts
@@ -1,14 +1,15 @@
-it('should test the request info', () => {
+it('should add, check and remove todo gracefully', () => {
+  //0. Initialize
   cy.visit('http://localhost:4200');
+  indexedDB.deleteDatabase('ngrx-todo-db');
+  //1. Add todo
   cy.get('#todo-title-input').type('Walk the dogs');
   cy.get('#add-todo-btn').click();
   cy.get('.todo-list').should('contain.text', 'Walk the dogs');
 
   cy.get('#check-todo-btn').click();
-
   cy.get('.todo-item').should('have.class', 'mat-primary');
 
   cy.get('#delete-todo-btn').click();
-
   cy.get('.todo-list').should('not.contain.text', 'Walk the dogs');
 });
